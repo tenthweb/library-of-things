9e36f38165db6a71ffa323bdc29e2efa97dcb749
tenthweb <47951574+tenthweb@users.noreply.github.com>
2026-02-22 11:53:56 +0000

    add psycopg2 to requirements to fix heroku crash

--- library_of_things/settings.py
+++ library_of_things/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dnh^^+8voina^9)tiibp#c9g^8ff)z-q(o8rpq^dus&+5lu-fi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1', 'localhost']
 
 
 # Application definition
